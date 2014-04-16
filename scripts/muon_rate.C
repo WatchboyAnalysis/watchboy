9c44d290b1046d42d6153ea10834874ccee8d5d5
morgan <maskins@ucdavis.edu>
2014-04-16 12:50:55 -0700

    hi

--- scripts/muon_rate.C
+++ scripts/muon_rate.C
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 
 void muon_rate(std::string fname)
 {
@@ -14,19 +15,44 @@ void muon_rate(std::string fname)
 
   unsigned long long tlast=0;
   unsigned int entries = tree->GetEntries();
-  double adc2us = 4/1000.0;
+  double adc2us = 4/1000.0;	// converts adc to microseconds
+  double axis_rescale = 1e-6;	// converts microseconds to seconds
+  double max_time = 5;		// seconds
 
-  TH1F* timing=new TH1F("timing", "timing", 100, 0, 5000);
+  TH1D* timing=new TH1D("timing", "timing", 100, 0, max_time);
 
   for(int i=0; i<entries; i++)
   {
     tree->GetEvent(i);
-    if( target_cb < 0.8 && target_total > 1000 )
+    if( target_cb < 0.15 && veto_cb < 0.45 && veto_total > 5e4 && target_total > 1e5 )
     {
-      timing->Fill(double(time-tlast)*adc2us);
+      timing->Fill(double(time-tlast)*adc2us*axis_rescale);
       tlast = time;
     }
   }
 
-  timing->Draw();
+  // Fit
+  TF1* fit = new TF1("fit", "[0]*TMath::Exp(-[1]*x)", 0, max_time);
+  fit->SetParameters(1000, 1);
+  timing->Fit(fit, "N");
+
+  // Legend
+  TLegend* l1 = new TLegend(0.55, 0.80, 0.88, 0.88);
+  stringstream ss;
+  ss << fit->GetParameter(1);
+  string leg = "Muon rate: "+(ss.str()).substr(0, 4)+" Hz";
+  l1->AddEntry(fit, leg.c_str());
+  l1->SetTextSize(0.04);
+
+  // Set up the timing parameters
+  gStyle->SetOptStat(0);
+  TCanvas* c1=new TCanvas();
+  c1->SetLogy();
+  timing->SetTitle("Time between muon events");
+  timing->GetXaxis()->SetTitle("Seconds");
+  fit->SetLineColor(kRed);
+  timing->SetLineColor(kBlack);
+  timing->Draw("E");
+  fit->Draw("same");
+  l1->Draw();
 }
